8f374d3f324b65d2d9bbcc9576aa43887e826501
sebas739 <sebastian7389@gmail.com>
2023-05-01 17:29:28 -0300

    parcial pagina de productos

--- js/productosjs.js
+++ js/productosjs.js
@@ -1,55 +1,55 @@
-let Products = [
-    {
-        name: 'PS5',
-        description: 'La consola más potente de Sony al día de la fecha',
-        price: 290000,
-        stock: true,
-        image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
-    },
-    {
-        name: 'PS4',
-        description: 'La consola anterior de Sony',
-        price: 140000,
-        stock: true,
-        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
-    },
-    {
-        name: 'PS3',
-        description: 'La consola anterior de Sony',
-        price: 70000,
-        stock: false,
-        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
+// let Products = [
+//     {
+//         name: 'PS5',
+//         description: 'La consola más potente de Sony al día de la fecha',
+//         price: 290000,
+//         stock: true,
+//         image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
+//     },
+//     {
+//         name: 'PS4',
+//         description: 'La consola anterior de Sony',
+//         price: 140000,
+//         stock: true,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
+//     },
+//     {
+//         name: 'PS3',
+//         description: 'La consola anterior de Sony',
+//         price: 70000,
+//         stock: false,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
       
-    },
-    {
-        name: 'PS2',
-        description: 'La consola anterior de Sony',
-        price: 30000,
-        stock: false,
-    },
-    {
-        name: 'XBOX Series X',
-        description: 'La consola más potente de Microsoft al día de la fecha',
-        price: 279000,
-        stock: true,
-        image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
+//     },
+//     {
+//         name: 'PS2',
+//         description: 'La consola anterior de Sony',
+//         price: 30000,
+//         stock: false,
+//     },
+//     {
+//         name: 'XBOX Series X',
+//         description: 'La consola más potente de Microsoft al día de la fecha',
+//         price: 279000,
+//         stock: true,
+//         image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
 
-    },
-    {
-        name: 'XBOX One',
-        description: 'La consola anterior de Microsoft',
-        price: 115000,
-        stock: false,
-        image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
-    },
-    {
-        name: 'XBOX 360',
-        description: 'La consola de Microsoft que compite con la PS3',
-        price: 60000,
-        stock: true,
-        image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
-    }
-]; 
+//     },
+//     {
+//         name: 'XBOX One',
+//         description: 'La consola anterior de Microsoft',
+//         price: 115000,
+//         stock: false,
+//         image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
+//     },
+//     {
+//         name: 'XBOX 360',
+//         description: 'La consola de Microsoft que compite con la PS3',
+//         price: 60000,
+//         stock: true,
+//         image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
+//     }
+// ]; 
 
 // EJEMPLO DE IMPRIMIR LO DEL LOCALSTORAGE
 swal({
